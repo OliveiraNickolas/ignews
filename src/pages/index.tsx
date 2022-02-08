d06ec30a1880c3b3f6c5386d330814d26af79ac5
Nickolas Oliveira <nickolasarthur@gmail.com>
2022-02-08 02:30:07 +0000

    Criada a estrutura do projeto e aplicado estilos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
+import Head from 'next/head';
+
+
 export default function Home() {
   return (
-    <h1>Oi Galera</h1>
+    <>
+
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+
+      <h1>
+        Hello World
+      </h1>
+
+    </>
   )
 }
