eaf8f2c200099bee0f1d24c3a8e6bf0875ed0913
Nickolas Oliveira <nickolasarthur@gmail.com>
2022-02-10 01:19:16 +0000

    Adicionado os componentes da página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,31 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
 
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <h1>
-        Hello World
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋 Hey, welcome!</span>
+          <h1>News abou the <br />
+          <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month subscription</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
 
     </>
   )
