52274c0a7c48bde90ddcfd1b3561b25d697170a2
Nickolas Oliveira <nickolasarthur@gmail.com>
2022-02-10 02:35:56 +0000

    Fundamentos do Next.js - Concluído

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import Head from 'next/head';
+
 import { SubscribeButton } from '../components/SubscribeButton';
+import { GetStaticProps } from 'next';
 
 import styles from './home.module.scss'
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number,
+  }
+}
+
+export default function Home({product} : HomeProps) {
   return (
     <>
 
@@ -19,9 +29,9 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month subscription</span>
+            <span>for {product.amount} month subscription</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Coding" />
@@ -29,4 +39,26 @@ export default function Home() {
 
     </>
   )
+}
+
+//Essa função abaixo está sendo executada no lado do node (back-end) que roda no next junto com nossa app, 
+// com ela nós vamos conseguir acessar as informações deste server-side para o front
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KRRPmLJM8tOEvaVFsOD7ibN')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100),
+    
+  }
+
+    return {
+    props: { 
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hrs
+  }
 }
